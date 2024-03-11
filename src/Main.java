80bc39ad27a97961aba806f362536ad3af27383f
Deva <jacobangel147@gmail.com>
2024-03-11 15:54:00 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -6,38 +6,57 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int score = 0;
 
-        Question questionOne = new Question("What is the primary cause of global warming?\n" +
-                "\n" +
-                " 1. Natural climate variability\n" +
-                " 2. Deforestation\n" +
-                " 3. Solar activity\n" +
-                " 4. Greenhouse gas emissions", 4, 3,4,3, 3 );
+        Question questionOne = new Question("""
+                  What is the primary cause of global warming?
+                                                                                            
+                
+                 1. Natural climate variability
+                                               
+                 2. Deforestation
+                                 
+                 3. Solar activity
+                                  
+                 4. Greenhouse gas emissions
+                 
+               """, 4, 3,4,3, 3 );
 
-        Question questionTwo = new Question( "Which gas is considered a major contributor to the greenhouse effect?\n" +
-                "\n" +
-                " 1. Nitrogen\n" +
-                " 2. Oxygen\n" +
-                " 3. Carbon Dioxide (CO2)\n" +
-                " 4. Methane (CH4)", 4, 3, 4, 3, 3 );
-        Question questionThree =new Question("What is the potential impact of global warming on sea levels?\n" +
-                "\n" +
-                " 1. Decrease in sea levels\n" +
-                " 2. No change in sea levels\n" +
-                " 3. Moderate rise in sea levels\n" +
-                " 4. Significant rise in sea levels",4,3,4,3,3);
-        Question  questionFour = new Question("Which of the following is a renewable energy source that can help mitigate global warming?\n" +
-                "\n" +
-                " 1. Coal\n" +
-                " 2. Natural Gas\n" +
-                " 3. Solar power\n" +
-                " 4. Nuclear power\n", 4,3,4,3,3);
+        Question questionTwo = new Question( """
+                Which gas is considered a major contributor to the greenhouse effect?
+                
+                " 1. Nitrogen
+                " 2. Oxygen
+                " 3. Carbon Dioxide (CO2)
+                " 4. Methane (CH4)
+                
+                """, 4, 3, 4, 3, 3 );
+        Question questionThree =new Question("""
+                What is the potential impact of global warming on sea levels?
+                
+                " 1. Decrease in sea levels
+                " 2. No change in sea levels
+                " 3. Moderate rise in sea levels
+                " 4. Significant rise in sea levels
+                
+                """,4,3,4,3,3);
+        Question  questionFour = new Question("""
+                Which of the following is a renewable energy source that can help mitigate global warming?
+                
+                " 1. Coal
+                " 2. Natural Gas
+                " 3. Solar power
+                " 4. Nuclear power
+                
+                """, 4,3,4,3,3);
 
-        Question questionFive =new Question("What is the consensus among scientists regarding human influence on global warming?\n" +
-                "\n" +
-                " 1. There is no consensus\n" +
-                " 2. Human activities have no impact\n" +
-                " 3. Human activities contribute to global warming\n" +
-                " 4. It's unclear and debated among scientists", 4,3,4,3,3 );
+        Question questionFive =new Question("""
+                What is the consensus among scientists regarding human influence on global warming?
+                
+                " 1. There is no consensus
+                " 2. Human activities have no impact
+                " 3. Human activities contribute to global warming
+                " 4. It's unclear and debated among scientists
+                
+                """, 4,3,4,3,3 );
 
         System.out.println(questionOne.questions);
         int firstChoice = input.nextInt();
--- src/Question.java
+++ src/Question.java
@@ -8,58 +8,19 @@ int fourthAnswer;
 int fifthAnswer;
 
 
-public Question(String questions, int firstAnswer, int secondAnswer, int thirdAnswer, int fourthAnswer, int fifthAnswer){
+public Question(String questions, int firstAnswer, int secondAnswer, int thirdAnswer, int fourthAnswer, int fifthAnswer) {
 
-this.questions = questions;
-this.firstAnswer = firstAnswer;
-this.secondAnswer = secondAnswer;
-this.thirdAnswer = thirdAnswer;
-this.fourthAnswer = fourthAnswer;
-this.fifthAnswer = fifthAnswer;
-
-}
-
-public void setQuestions(){
     this.questions = questions;
-}
-
-public void setFirstAnswer(){
     this.firstAnswer = firstAnswer;
-}
-public void setSecondAnswer(){
     this.secondAnswer = secondAnswer;
-}
-public void  setThirdAnswer(){
     this.thirdAnswer = thirdAnswer;
-}
-public void setFourthAnswer(){
     this.fourthAnswer = fourthAnswer;
-}
-public void setFifthAnswer(){
     this.fifthAnswer = fifthAnswer;
 }
 
+}
 
 
-    public String getQuestions(){
-    return questions;
-}
-public int getFirstAnswer(){
-    return firstAnswer;
-}
-    public int getSecondAnswer() {
-        return secondAnswer;
-    }
-    public int getThirdAnswer() {
-        return thirdAnswer;
-    }
-    public int getFourthAnswer(){
-    return fourthAnswer;
-    }
-    public int getFifthAnswer() {
-        return fifthAnswer;
-    }
 
-}
 
 
